0713a1de3e80ca5fea2f47d0798137d120c068f6
TKDevgirl <144117454+TKDevgirl@users.noreply.github.com>
2026-07-09 19:39:10 +0700

    Fix static files for layout

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.api import racks, rooms, equipment, dashboard, buildings, floors, layout, web
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="PMIS Enterprise API",
     version="1.0.0"
 )
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(racks.router)
 app.include_router(rooms.router)
 app.include_router(equipment.router)
